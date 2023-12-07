4ee68f1d439966ff513a195fd98c7bd6e0544f50
Suto <108921554+ahmadthoba@users.noreply.github.com>
2023-12-07 12:02:08 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return view('pages.blank-page', ['type_menu' => '']);
+    return view('auth.login', ['type_menu' => '']);
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
